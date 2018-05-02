02ada5bb31ac8ab1e7e120ae76ec7993f4cef662
Arthur J Schwartz <arthurjschwartz@lynnes-imac.local>
2018-05-02 06:05:51 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Arthur J Schwartz on 5/2/18.
+//  Copyright © 2018 Arthur J Schwartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
